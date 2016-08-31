67109915e81fd63353c753f762666f79a9ed0f29
姚哥 <yaoge.com>
2016-08-31 13:40:46 +0800

    添加表格视图，绑定假数据

--- WYNews/WYNews/Classes/Home/NewsList/WYNewsListViewController.m
+++ WYNews/WYNews/Classes/Home/NewsList/WYNewsListViewController.m
@@ -7,8 +7,9 @@
 //
 
 #import "WYNewsListViewController.h"
-
-@interface WYNewsListViewController ()
+static NSString *cellId = @"cellId";
+@interface WYNewsListViewController ()<UITableViewDelegate, UITableViewDataSource>
+@property(nonatomic, weak) UITableView *tableView;
 
 @end
 
@@ -16,7 +17,42 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.view.backgroundColor = [UIColor greenColor];
+    [self setupUI];
+}
+
+#pragma mark -UITableViewDataSource
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 20;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellId forIndexPath:indexPath];
+    cell.textLabel.text = @(indexPath.row).description;
+    return cell;
+
+}
+
+#pragma mark -设置界面
+
+- (void)setupUI {
+    // 添加表格
+    UITableView *tv = [[UITableView alloc] initWithFrame:CGRectZero style:UITableViewStylePlain];
+    
+    [self.view addSubview: tv];
+    
+    // 自动布局
+    [tv mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.edges.equalTo(self.view);
+    }];
+    
+    [tv registerClass:[UITableViewCell class] forCellReuseIdentifier:cellId];
+    
+    tv.dataSource = self;
+    tv.delegate = self;
+    
+    _tableView = tv;
 }
 
 - (void)didReceiveMemoryWarning {
